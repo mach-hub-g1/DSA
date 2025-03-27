3cd2a94cd5bf16e3eccc977e8303a688bd243b17
GAJENDRA SINGH <gajendrasingh989895@gmail.com>
2025-03-27 23:19:30 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int a ;
+    cout<< "enter the value :";
+    cin >> a;
+    cout<< ( a >= 0  ? "positive" : "negative") <<  endl;
+   
+ 
+    return 0;
+}   
